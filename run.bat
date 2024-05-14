84b728758e3c6f75893893335ecf2eba56caab9e
Ex-voda <lsh301705@gmail.com>
2024-05-14 22:40:47 +0800

    fix

--- run.bat
+++ run.bat
@@ -1,5 +1,11 @@
 @echo off
 call conda.bat activate base
-start powershell -Command "python flask/src/app.py; pause"
+start cmd /c "python flask/src/app.py & pause"
 
-start powershell -Command "cd web; npm run dev; pause"
+start cmd /c "cd web && npm run dev & pause"
+
+REM 等待一段时间以确保服务器已启动
+timeout /t 3
+
+REM 使用 Edge 浏览器打开指定的 URL
+start msedge http://localhost:3000
